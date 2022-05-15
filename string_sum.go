c9cabda7fd08bef3f64fd8998e1868d9033e3eec
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-05-15 13:18:47 +0400

    ""

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -12,16 +15,51 @@ var (
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
-
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	t := input
+
+	t = strings.TrimSpace(t)
+
+	if len(t) == 0 {
+		return "", errorEmptyInput
+	}
+
+	fmt.Println(t)
+	isFirstNegative := strings.HasPrefix(t, "-")
+
+	arr := strings.Split(t, "-")
+	splitNum := len(arr)
+
+	if isFirstNegative {
+		splitNum--
+	}
+
+	if splitNum == 2 {
+		first, errFirst := strconv.Atoi(strings.TrimSpace(arr[len(arr)-2]))
+
+		second, errSecond := strconv.Atoi(strings.TrimSpace(arr[len(arr)-1]))
+
+		if errFirst == nil && errSecond == nil {
+			if isFirstNegative {
+				first *= -1
+			}
+			return strconv.Itoa(first - second), nil
+		}
+
+	}
+
+	arr = strings.Split(t, "+")
+	splitNum = len(arr)
+
+	if splitNum == 2 {
+		first, errFirst := strconv.Atoi(strings.TrimSpace(arr[len(arr)-2]))
+
+		second, errSecond := strconv.Atoi(strings.TrimSpace(arr[len(arr)-1]))
+
+		if errFirst == nil && errSecond == nil {
+			return strconv.Itoa(first + second), nil
+		}
+	}
+
+	return "", errorNotTwoOperands
 }
